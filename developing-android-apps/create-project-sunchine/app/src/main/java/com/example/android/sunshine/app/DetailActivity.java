12cbe25db0671385b8a9dec28b70327f9c536fbb
Fernando Hidalgo <fernando.hidalgo@smartmatic.com>
2017-04-03 14:14:08 -0500

    #N Launch SettingsActivity.
    - Exists a PreferencesActivity that has the behavior by default of many controls.

--- developing-android-apps/create-project-sunchine/app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ developing-android-apps/create-project-sunchine/app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -41,6 +41,8 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settings = new Intent(this, SettingsActivity.class);
+            startActivity(settings);
             return true;
         }
 
