4359d6111135ccd740ea8c38153b744a0cc199cb
Fernando Hidalgo <fechidal89@gmail.com>
2017-04-05 22:41:27 -0500

    #M Temperature Units Setting
    You can change the unit on Settings and automatically it will show in the MainActivity.

--- developing-android-apps/create-project-sunchine/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ developing-android-apps/create-project-sunchine/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -32,6 +32,8 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import static android.content.ContentValues.TAG;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -128,6 +130,20 @@ public class ForecastFragment extends Fragment {
      * Prepare the weather high/lows for presentation.
      */
     private String formatHighLows(double high, double low) {
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String unitType = sharedPreferences.getString(
+                getString(R.string.pref_units_key),
+                getString(R.string.pref_units_metric));
+
+        if (unitType.equals(getString(R.string.pref_units_imperial))){
+            high = (high * 1.8) + 32;
+            low = (low * 1.8) + 32;
+        }else if (!unitType.equals(getString(R.string.pref_units_metric))){
+            Log.e(TAG, "Unit type not found: " + unitType);
+        }
+
         // For presentation, assume the user doesn't care about tenths of a degree.
         long roundedHigh = Math.round(high);
         long roundedLow = Math.round(low);
